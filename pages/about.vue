e5f44a1366c5b0a50a0d543a8b2f3a2f2d79cc76
usmanlubis <usmanlubis11@gmail.com>
2023-08-28 14:25:37 +0700

    create about and contact page

---
+++ pages/about.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <HeaderComponent></HeaderComponent>
+    <main>
+      <h1>This is About page</h1>
+    </main>
+    <FooterComponent></FooterComponent>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'IndexPage'
+  }
+</script>
+
+<style>
+  *,
+  *::before,
+  *::after {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #282A35;
+    min-height: 100vh;
+    padding-bottom: 40px;
+  }
+
+  main {
+    color: whitesmoke;
+    text-align: center;
+    margin-top: 4em;
+  }
+</style>
