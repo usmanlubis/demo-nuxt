cb34fd3cc0d25e275ce17c51f4929fc0aa23a4bb
usmanlubis <usmanlubis11@gmail.com>
2023-08-28 13:44:06 +0700

    Initial commit

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+}
+</script>
+
+<style>
+  header {
+    padding: 1.5em 3em;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  ul {
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    gap: 2em;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <nav>
+      <div>
+        <h2>Tutorial NuxtJS</h2>
+      </div>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'IndexPage'
+  }
+</script>
+
+<style>
+  header {
+    padding: 1.5em 3em;
+  }
+
+  nav {
+    display: flex;
+    justify-content: end;
+  }
+
+  ul {
+    display: flex;
+    /* justify-content: end; */
+    align-items: center;
+    gap: 2em;
+  }
+
+  li {
+    list-style-type: none;
+  }
+</style>
