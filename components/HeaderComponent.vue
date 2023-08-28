c0a47a9106c67e148261b09761d776cd7c834786
usmanlubis <usmanlubis11@gmail.com>
2023-08-28 14:13:35 +0700

    edit header

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -2,12 +2,12 @@
   <header>
     <nav>
       <div>
-        <h2>Tutorial NuxtJS</h2>
+        <h2>Demo NuxtJS</h2>
       </div>
       <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/contact">Contact</router-link></li>
+        <li><router-link class="nav-link" to="/">Home</router-link></li>
+        <li><router-link class="nav-link" to="/about">About</router-link></li>
+        <li><router-link class="nav-link" to="/contact">Contact</router-link></li>
       </ul>
     </nav>
   </header>
@@ -21,22 +21,39 @@
 
 <style>
   header {
-    padding: 1.5em 3em;
+    padding: 1em 3em;
+    background-color: whitesmoke;
   }
 
   nav {
     display: flex;
-    justify-content: end;
+    justify-content: space-between;
   }
 
   ul {
     display: flex;
-    /* justify-content: end; */
     align-items: center;
-    gap: 2em;
+    gap: 1em;
   }
 
   li {
     list-style-type: none;
   }
+
+  .nav-link {
+    text-decoration: none;
+    color: black;
+    font-size: 1.2em;
+    padding: .2em 1em;
+    border-radius: 1em;
+  }
+
+  .nav-link:hover{
+    background-color: rgba(0, 0, 0, .1);
+  }
+
+  .nav-link:active {
+    opacity: .8;
+  }
+
 </style>
