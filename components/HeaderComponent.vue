e3834709f47b5e68b13fa592522c5823036609eb
usmanlubis <usmanlubis11@gmail.com>
2023-08-28 13:48:33 +0700

    add header component

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,6 +1,9 @@
 <template>
   <header>
     <nav>
+      <div>
+        <h2>Tutorial NuxtJS</h2>
+      </div>
       <ul>
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/about">About</router-link></li>
@@ -11,23 +14,29 @@
 </template>
 
 <script>
-export default {
-  name: 'HeaderComponent'
-}
+  export default {
+    name: 'HeaderComponent'
+  }
 </script>
 
 <style>
   header {
     padding: 1.5em 3em;
+  }
+
+  nav {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    justify-content: end;
   }
 
   ul {
     display: flex;
-    justify-content: end;
+    /* justify-content: end; */
     align-items: center;
     gap: 2em;
+  }
+
+  li {
+    list-style-type: none;
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,5 @@
 <template>
-  <header>
-    <nav>
-      <div>
-        <h2>Tutorial NuxtJS</h2>
-      </div>
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/contact">Contact</router-link></li>
-      </ul>
-    </nav>
-  </header>
+  <HeaderComponent></HeaderComponent>
 </template>
 
 <script>
@@ -20,23 +9,5 @@
 </script>
 
 <style>
-  header {
-    padding: 1.5em 3em;
-  }
 
-  nav {
-    display: flex;
-    justify-content: end;
-  }
-
-  ul {
-    display: flex;
-    /* justify-content: end; */
-    align-items: center;
-    gap: 2em;
-  }
-
-  li {
-    list-style-type: none;
-  }
 </style>
