b952a4aa25dff24e4e83ba89fdc535c95eb51aed
usmanlubis <usmanlubis11@gmail.com>
2023-08-28 14:13:24 +0700

    add footer

---
+++ components/FooterComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <p>&copy; {{ new Date().getFullYear() }} Usman Lubis - All rights reserved. Made with Nuxt3</p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'FooterComponent',
+  }
+</script>
+
+<style>
+  footer {
+    height: 40px;
+    background-color: whitesmoke;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
