e143f90db982eabf09e813947610c0eb2f968504
usmanlubis <usmanlubis11@gmail.com>
2023-08-28 14:16:58 +0700

    create default layouts

---
+++ layouts/dafault.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+
+    <Nuxt />
+
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+  import HeaderComponent from '@/components/HeaderComponent';
+  import FooterComponent from '@/components/FooterComponent';
+
+  export default {
+    components: {
+      HeaderComponent,
+      FooterComponent
+    }
+  }
+</script>
+
+<style>
+
+</style>
